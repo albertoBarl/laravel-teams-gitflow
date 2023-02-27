fa9fc874bc12ffc18d5b52a8d7c279b0ea6b3a78
albertoBarl <alberto.barletta00@gmail.com>
2023-02-27 19:10:24 +0100

    index connected

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <div class="d-flex flex-column justify-content-center align-items-center p-5">
         <p>CLICK THE BUTTON BELOW TO SEE OUR CATALOGUE</p>
-        <a href="" class="btn btn-success">OPEN UP!</a>
+        <a href="{{ route('movies.index') }}" class="btn btn-success">OPEN UP!</a>
     </div>
 @endsection
