b3b3f190712cdb386432881a0f0de6304aa98aa0
mattiabenelli <benellimattia0@gmail.com>
2023-03-02 16:20:03 +0100

    page show done

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>Ciao sono la show {{ $item['title'] }}</div>
+<section class="py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="row gx-4 gx-lg-5 align-items-center">
+            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{$item['cover_path']}}" alt="{{$item['title']}}" /></div>
+            <div class="col-md-6">
+                <h1 class="display-5 fw-bolder">{{$item['title']}}</h1>
+                <div class="fs-5 mb-5">
+                    <span class="">{{$item['original_title']}}</span>
+                </div>
+                <p class="lead">{{$item['cast']}}</p>
+                <div class="d-flex">
+                    <button class="btn btn-warning me-2">
+                        <i class="fa-regular fa-pen-to-square"></i>
+                    </button>
+                    <button class="btn btn-danger">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
