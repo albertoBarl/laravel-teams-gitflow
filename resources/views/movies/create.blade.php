6ce6dcd6be5a6a43b33197989103ed549bdff66e
albertoBarl <alberto.barletta00@gmail.com>
2023-03-02 17:15:14 +0100

    fixed inputs

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 @section('content')
     <div class="container">
@@ -9,58 +8,62 @@
             <div class="class-12">
                 @if ($errors->any())
                     @foreach ($errors->all() as $error)
-                        <div class="alert alert-danger">{{$error}}</div>
+                        <div class="alert alert-danger">{{ $error }}</div>
                     @endforeach
                 @endif
-                <form method="POST" action="{{route('movies.store')}}">
+                <form method="POST" action="{{ route('movies.store') }}">
                     @csrf
                     <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="title">Title</label>
                         <input type="text" class="form-control" name="title" id="title" placeholder="Add title">
                         @error('title')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="original_title">Original Title</label>
-                        <input type="text" class="form-control" name="original_title" id="original_title" placeholder="Add original title">
+                        <input type="text" class="form-control" name="original_title" id="original_title"
+                            placeholder="Add original title">
                         @error('original_title')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-					<div class="form-group m-2">
+                    <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="nationality">Nationality</label>
-                        <input type="text" class="form-control" name="nationality" id="nationality" placeholder="Add nationality">
+                        <input type="text" class="form-control" name="nationality" id="nationality"
+                            placeholder="Add nationality">
                         @error('nationality')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-					<div class="form-group m-2">
+                    <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="release_date">Release Date</label>
-                        <input type="text" class="form-control" name="release_date" id="release_date" placeholder="Add release date">
+                        <input type="date" class="form-control" name="release_date" id="release_date"
+                            placeholder="Add release date">
                         @error('release_date')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-					<div class="form-group m-2">
+                    <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="vote">Vote</label>
-                        <input type="text" class="form-control" name="vote" id="vote" placeholder="Add vote">
+                        <input type="number" class="form-control" name="vote" id="vote" placeholder="Add vote">
                         @error('vote')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-					<div class="form-group m-2">
+                    <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="cast">Cast</label>
                         <input type="text" class="form-control" name="cast" id="cast" placeholder="Add cast">
                         @error('cast')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-					<div class="form-group m-2">
+                    <div class="form-group m-2">
                         <label class="fs-2 fw-semibold" for="cover_path">Cover Path</label>
-                        <input type="text" class="form-control" name="cover_path" id="cover_path" placeholder="Add URL image">
+                        <input type="text" class="form-control" name="cover_path" id="cover_path"
+                            placeholder="Add URL image">
                         @error('cover_path')
-                        <div class="text-danger">{{$message}}</div>
+                            <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <button type="submit" class="btn btn-success">Save</button>
