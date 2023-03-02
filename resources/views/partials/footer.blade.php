7dd511e554dc052278ba2073b59ff230388360f7
albertoBarl <alberto.barletta00@gmail.com>
2023-03-02 16:31:13 +0100

    styled footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,6 @@
-<footer id="footer" class="bg-dark text-center text-white">
-    <p>Powered by M.Benelli, A.Barile, F.Battaglia, F,Dadamo, A.Barletta</p>
+<footer id="footer" class="bg-dark text-center text-white p-5">
+    {{-- <div id="logo"><img src="{{ Vite::asset('public/images/logo.png') }}" alt="logo not available"></div> --}}
+
+    <p>Powered by <a href="">M.Benelli</a>, <a href="">A.Barile</a>, <a href="">F.Battaglia</a>, <a
+            href="">F.Dadamo</a>, <a href="">A.Barletta</a></p>
 </footer>
