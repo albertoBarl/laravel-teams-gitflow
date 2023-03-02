57a6e0db187b6640b0fa06e0a5496767af22a0c9
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-02 17:52:48 +0100

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController as PagesController;
-use App\Http\Controllers\MoviesController as MoviesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,11 +9,25 @@ use App\Http\Controllers\MoviesController as MoviesController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/', [PagesController::class, 'index'])->name('homepage');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('movies', MoviesController::class);
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/movies', MoviesController::class)->parameters(['movies'=>'movie:id']);
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
