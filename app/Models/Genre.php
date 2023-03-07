15b366821ad05a2ab4e07c0a6f2df384ce5633c7
Fra-Battaglia <francescobattaglia03@gmail.com>
2023-03-07 16:35:24 +0100

    one to many relation created

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Movie;
 
 class Genre extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function movies() {
+        return $this->hasMany(Movie::class);
+    }
 }
