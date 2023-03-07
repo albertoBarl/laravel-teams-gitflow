7e8341d7916b4f020b086c942bfb00e46218f7ae
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-07 16:17:47 +0100

    aggiunto seeder

--- database/seeders/ActorSeeder.php
+++ database/seeders/ActorSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// MODEL
+use App\Models\Actor;
+
 class ActorSeeder extends Seeder
 {
     /**
@@ -14,6 +17,60 @@ class ActorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $actors = 
+        [
+            'Marlon Brando', 
+            'Al Pacino', 
+            'James Caan', 
+            'Robert Duval', 
+            'Viggo Mortensen', 
+            'Elijah Wood', 
+            'Sean Astin', 
+            'Ian McKellen', 
+            'Andy Serkis', 
+            'Liv Tyler', 
+            'Christopher Lee', 
+            'Hugo Weaving', 
+            'Karl Urban', 
+            'Cate Blanchett',
+            'Lacey Duvall', 
+            'Jack Nicholson',
+            'Sandra Bullock', 
+            'George Clooney', 
+            'Ed Harris',
+            'Natalie Portman', 
+            'Stephen Rea',
+            'John Travolta',
+            'Samuel L Jackson', 
+            'Bruce Willis', 
+            'Uma Thurman',
+            'Tom Hanks', 
+            'Robin Wright', 
+            'Gary Sinise', 
+            'Mykelti Williamson', 
+            'Sally Field',
+            'Mark Hammil', 
+            'Harrison Ford', 
+            'Carrie Fisher', 
+            'Billy Dee Williams', 
+            'David Prowse',
+            'Henry Thomas',
+            'Drew Barrymore',
+            'Johnny Depp', 
+            'Christina Ricci', 
+            'Miranda Richardson', 
+            'Michael Gambon', 
+            'Christopher Walken',
+            'Tom Cruise', 
+            'Nicole Kidman'
+        ];
+
+        foreach($actors as $actor){
+            $newActor = new Actor();
+            $newActor->name = $actor;
+            $newActor->slug = Actor::generateSlug($newActor->name);
+
+            $newActor->save();
+        }
     }
 }
